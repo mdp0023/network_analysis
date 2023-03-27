3e9d5b96d1d11d73ce72475185c04ca1afc0c9d3
mdp0023 <mattpreisser@gmail.com>
2023-03-27 11:07:05 -0500

    updates to resource data acquisition algorithm

--- resource_data.py
+++ resource_data.py
@@ -39,11 +39,17 @@ pd.set_option('display.max_columns', None)
 ########################################################################
 
 ########################################################################
-def resource_centroids(boundary, resources = 'default', custom_r=None, disused_filter = True, save=None, cache=False):
+def resource_centroids(boundary, 
+                       resources = 'default', 
+                       custom_r=None, 
+                       disused_filter = True, 
+                       save=None,
+                       prefix=None, 
+                       cache=False):
     """
-    A function to pull resource centroids Open Street Map.
+    A function to pull resource centroids from Open Street Map.
     
-    Utilizes functions already created in OSMNx. This function should streamline the data pulling process ny combining the resource that have identified as being critical.
+    Utilizes functions already created in OSMNx. This function should streamline the data pulling process by combining the resource that have identified as being critical. Each resource is saved as a seperate unique shapefile with the given projection.
 
     REMEMBER: Input boundary shapefile should be projected, and code should correctly manage projections.
 
@@ -55,12 +61,14 @@ def resource_centroids(boundary, resources = 'default', custom_r=None, disused_f
     :type custom_r: list of key-value pairs
     :param disused_filter: If True, will filter out resources that have an input for disused column, signifiying that it is most likely closed
     :type disused_filter: bool
-    :param save: If not None, save to file path
+    :param save: If not None, this is a string matching folder location/name that contains where shapefiles should go
     :type save: string
+    :param prefix: prefix to save file name. Suffix of each file will be the value of the key-value pair passed to osmnx (i.e., the resource type/name)
+    :type prefix: string
     :param cache: sets OSMNx cache setting - if cache folder exists, and need new data, must be set to False. Default is False, set to True if repeatedly pulling same information, it will increase speed.
     :type cache: bool
     
-    :return: **resource_points**, geopandas shapefile of centroids of resource locations within boundary
+    :return: **resource_points**, geopandas shapefile of centroids of all resource locations within boundary
     :rtype: geopandas shapefile
 
     """
@@ -76,9 +84,12 @@ def resource_centroids(boundary, resources = 'default', custom_r=None, disused_f
     # determine list of key value pairs
     if resources == 'default':
         resource_list = [{'shop': 'supermarket'}, 
-                         {'amenity': 'fuel'}, {'shop': 'convenience'},
-                         {'healthcare': 'hospital'}, {'amenity':'pharmacy'},
-                         {'amenity': 'fire_station'}, {'emergency': 'ambulance_station'},
+                         {'amenity': 'fuel'}, 
+                         {'shop': 'convenience'},
+                         {'healthcare': 'hospital'}, 
+                         {'amenity':'pharmacy'},
+                         {'amenity': 'fire_station'}, 
+                         {'emergency': 'ambulance_station'},
                          {'amenity':'police'}]
 
     elif resources == 'custom':
@@ -89,101 +100,124 @@ def resource_centroids(boundary, resources = 'default', custom_r=None, disused_f
 
     # iterate through key value pairs
     for key_value in resource_list:
-        data = ox.geometries_from_polygon(geo['geometry'][0], tags=key_value).to_crs(crs_proj)
+        # if healthcare, only interested in hospitals with emergency room capabilities
+        if list(key_value.keys())[0] == 'healthcare':
+            data = ox.geometries_from_polygon(geo['geometry'][0], tags=key_value).to_crs(epsg=aoi_crs)
+            data = data[data['emergency'] == 'yes']
+        else:
+            data = ox.geometries_from_polygon(geo['geometry'][0], tags=key_value).to_crs(crs_proj)
         data_points = data.set_geometry(data.centroid)
         data_points['Resource'] = list(key_value.values())[0]
     
-    # Merge data into singular dataframe
-        resource_points = pd.concat([resource_points,data_points])
+        # drop columns that has a list in - for some reason throwing an error
+        for col in data_points.columns:
+            if any(isinstance(val, list) for val in data_points[col]):
+                #print(f'Column: {col}, has a list in it')
+                data_points = data_points.drop(labels=col, axis='columns')
 
-    # drop columns that has a list in - for some reason throwing an error
-    for col in resource_points.columns:
-        if any(isinstance(val, list) for val in resource_points[col]):
-            #print(f'Column: {col}, has a list in it')
-            resource_points = resource_points.drop(labels=col, axis='columns')
+        # filter out disused locations
+        if disused_filter is True:
+            # find columns in output that begin with disused 
+            # Do this incase it is not disused_sh (shop) like for supermarkets
+            filter_col = [col for col in data_points if col.startswith('disused')]
+            for col in filter_col:
+                data_points = data_points[data_points[col].isnull()]
 
-    # filter out disused locations
-    if disused_filter is True:
-        # find columns in output that begin with disused 
-        # Do this incase it is not disused_sh (shop) like for supermarkets
-        filter_col = [col for col in resource_points if col.startswith('disused')]
-        for col in filter_col:
-            resource_points = resource_points[resource_points[col].isnull()]
+        # save file if necessary
+        if save is not None:
+            data_points.to_file(f'{save}/{prefix}_{list(key_value.values())[0]}_points.shp')
 
-    # save file if necessary
-    if save is not None:
-        resource_points.to_file(save)
+        # Merge data into singular dataframe
+        resource_points = pd.concat([resource_points, data_points])
 
     return resource_points
 
 
-def resource_parcels(points, parcels, save):
+def resource_parcels(points, 
+                     parcels, 
+                     fpath,
+                     prefix):
     """Extract parcels given known set of centroids.
     
     Parcel data is not regularly available in OSMNx. This function does a simple relate of the centroid points and parcel file to extract the associated resource parcels. The output resource parcel file will contain an attribute with information on what and how many resources are located within each boundary. Some parcels will house multiple resources (of the same type of different type) and the 'Resources' attribute should provide that information.
 
-    :param points: Filepath to .shp file of points/centroids of interest. Should have projected coordinates of study area
-    :type points: string
+    :param points: List of the key value pairs associated with the osmnx data pull 
+    :type points: list
     :param parcels: Filepath to .shp file of parcels of interest. Should have projected coordinates of study area
     :type parcels: string 
+    :param save: folder location for where shapefiles should be saved, and where the point files are located
+    :type save: string
+    :param prefix: prefix for saved shapefiles
+    :type prefix: string
     
     :return: **resource_parcels**, geopandas shapefile of parcels of resource locations
     :rtype: geopandas shapefile
     
     """
-    points = gpd.read_file(points)
+    
     parcels = gpd.read_file(parcels)
 
-    # get unique resource names
-    unique_res = points['Resource'].unique()
-    #spatial join 
-    dfsjoin = gpd.sjoin(parcels, points)
-    dfpivot = pd.pivot_table(dfsjoin, index='GEO_ID', columns='Resource',aggfunc={'Resource':len})
-    dfpivot.columns = dfpivot.columns.droplevel()
-    dfpolynew = parcels.merge(dfpivot, how='left', on='GEO_ID')
+    # iterate through key value pairs
+    for key_value in points:
+        point = gpd.read_file(f'{fpath}/{prefix}_{list(key_value.values())[0]}_points.shp')
 
-    # create a new column that is the total sum of all resources
-    dfpolynew['Resource_Sum'] = dfpolynew[unique_res].sum(axis=1)
-   
-    # create new resource parcel data layer
-    resource_parcels = dfpolynew[dfpolynew['Resource_Sum'] > 0]
+        # get unique resource names
+        unique_res = point['Resource'].unique()
+        #spatial join 
+        dfsjoin = gpd.sjoin(parcels, point)
+        dfpivot = pd.pivot_table(dfsjoin, index='GEO_ID', columns='Resource',aggfunc={'Resource':len})
+        dfpivot.columns = dfpivot.columns.droplevel()
+        dfpolynew = parcels.merge(dfpivot, how='left', on='GEO_ID')
 
-    if save is not None:
-        resource_parcels.to_file(save)
+        # create a new column that is the total sum of all resources
+        dfpolynew['Resource_Sum'] = dfpolynew[unique_res].sum(axis=1)
     
-    pass
+        # create new resource parcel data layer
+        resource_parcels = dfpolynew[dfpolynew['Resource_Sum'] > 0]
+
+        resource_parcels.to_file(
+            f'{fpath}/{prefix}_{list(key_value.values())[0]}_parcels.shp')
+
+    
 
 
 
 ########################################################################
 # VARIABLES
 place = 'Austin, Texas, USA'
-aoi_crs = 32614
+aoi_crs = 26914 # NAD83 UTM zone 14N
 folder = '/home/mdp0023/Desktop/external/Data/Network_Data'
-SC_boundary_file = f'{folder}/Shoal_Creek/SC_Boundary/SC_Boundary.shp'
+# SC_boundary_file = f'{folder}/Shoal_Creek/SC_Boundary/SC_Boundary.shp'
 AN_boundary_file = f'{folder}/Austin_North/AN_Boundary/AN_Boundary.shp'
-BPA_boundary_file = f'{folder}/Beaumont_Port_Arthur/BPA_Boundary/BPA_Boundary.shp'
+# BPA_boundary_file = f'{folder}/Beaumont_Port_Arthur/BPA_Boundary/BPA_Boundary.shp'
 
-SC_output = f'{folder}/Shoal_Creek/SC_Resource_Parcel_Shapefiles/SC_Supermarket_Parcel_Points.shp'
-AN_output = f'{folder}/Austin_North/AN_Resource_Parcel_Shapefiles/AN_Supermarket_Parcel_Points.shp'
-BPA_output = f'{folder}/Beaumont_Port_Arthur/BPA_Resource_Parcel_Shapefiles/BPA_Supermarket_Parcel_Points.shp'
-
-AN_parcel_output = f'{folder}/Austin_North/AN_Resource_Parcel_Shapefiles/AN_Supermarket_Parcel.shp'
-BPA_parcel_output = f'{folder}/Beaumont_Port_Arthur/BPA_Resource_Parcel_Shapefiles/BPA_Supermarket_Parcel.shp'
+# SC_output = f'{folder}/Shoal_Creek/SC_Resource_Parcel_Shapefiles/SC_Supermarket_Parcel_Points.shp'
+AN_output = f'{folder}/Austin_North/AN_Resource_Parcel_Shapefiles'
+# BPA_output = f'{folder}/Beaumont_Port_Arthur/BPA_Resource_Parcel_Shapefiles/BPA_Supermarket_Parcel_Points.shp'
 
 AN_parcels = '/home/mdp0023/Desktop/external/Data/Boundaries/Parcels/Travis_County_Parcels_2019/shp/Travis_County_Parcels_2019.shp'
-BPA_parcels = '/home/mdp0023/Desktop/external/Data/Boundaries/Parcels/Beaumont_Port_Arthur_Parcels_2022/BPA_Residential_Parcels_Shapefiles.shp'
+# BPA_parcels = '/home/mdp0023/Desktop/external/Data/Boundaries/Parcels/Beaumont_Port_Arthur_Parcels_2022/BPA_Residential_Parcels_Shapefiles.shp'
 
-custom_r = [{'shop': 'supermarket'}]
+custom_r = [{'amenity': 'fuel'},
+            {'shop': 'convenience'},
+            {'healthcare': 'hospital', 'emergency': 'yes'},
+            {'amenity': 'pharmacy'},
+            {'amenity': 'fire_station'},
+            {'emergency': 'ambulance_station'},
+            {'amenity': 'police'}]
 ########################################################################
-# resource_points = resource_centroids(boundary = BPA_boundary_file,
+# resource_points = resource_centroids(boundary=AN_boundary_file,
 #                                     resources = 'custom',
 #                                     custom_r = custom_r,
 #                                     disused_filter = True,
-#                                     save=BPA_output)
+#                                     save=AN_output,
+#                                     prefix='AN')
 
 
-resource_parcels(points=BPA_output, parcels=BPA_parcels, save=BPA_parcel_output)
+resource_parcels(points=custom_r, 
+                 parcels=AN_parcels,
+                 fpath=AN_output,
+                 prefix='AN')
 
 
 ####################################################################################
