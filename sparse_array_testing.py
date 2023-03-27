503741eefc8d1b61ce046a2c1552b87f7a7751dc
mdp0023 <mattpreisser@gmail.com>
2023-03-27 11:06:53 -0500

    More testing of sparse arrays

--- sparse_array_testing.py
+++ sparse_array_testing.py
@@ -1,34 +1,159 @@
 # this is where I can mess with sparse matrices to see how they are manipulated
 
+from numpy import multiply
+from scipy.sparse import csr_matrix
 import numpy as np
 from scipy import sparse
 import time
 import sys
 from scipy.sparse import random
+import scipy.sparse._sparsetools as _sparsetools
+from scipy.sparse._sputils import (get_index_dtype, upcast)
+from multiprocessing import Pool
 
-data1 = np.array([[2,0,0,0,5],
+
+data1 = np.array([[0,0,0,0,5],
                   [0,0,0,3,0],
                   [0,0,7,0,0],
                   [4,5,6,7,0],
                   [0,8,0,2,0]])
-data2 = np.array([[2, 0, 0, 0, 5],
+data2 = np.array([[600, 0, 0, 0, 10],
+                  [0, 0, 0, 6, 0],
+                  [0, 0, 14, 0, 0],
+                  [8, 10, 12, 14, 0],
+                  [0, 16, 0, 4, 0]])
+data3 = np.array([[17, 0, 0, 0, 5],
                   [0, 0, 0, 3, 0],
                   [0, 0, 7, 0, 0],
-                  [4, 5, 6, 7, 0],
-                  [0, 8, 0, 2, 0]])
-data3 = np.array([[np.inf, 0, 0, 0, 5],
-                  [0, 0, 0, 3, 0],
-                  [0, 0, 7, 0, 0],
-                  [4, 5, 6, 7, 0],
-                  [0, 8, 0, np.inf, 0]])
+                  [4, 12, 19, 7, 0],
+                  [0, 8, 0, 15, 0]])
+
+
+
+sparse1 = sparse.lil_array(data1)
+for i, row in enumerate(sparse1.rows):
+    for j in row:
+        print(i,j)
+
+
+sparse1 = sparse.csr_array(data1,dtype='float32')
+sparse2 = sparse.csr_array(data2,dtype='float32')
+sparse3 = sparse.csr_array(data3, dtype='float32')
+addition = sparse.csr_array(
+    ([1 for i in sparse2.data], sparse2.indices, sparse2.indptr))
+alpha_array=0.15
+beta_array=4
 
 
 
 
 
+weight_array_iter = sparse3.multiply(sparse1.multiply(
+    sparse2.power(-1)).power(beta_array).multiply(alpha_array)+addition)
+# print(weight_array_iter)
+
+
+weight_array_iter = sparse3 * \
+    (1+alpha_array*(sparse1/sparse2)**beta_array)
+# print(weight_array_iter)
+
+
+# individual math test
+sparse1 = sparse.rand(15000, 15000, density=0.0002, format='lil')
+sparse2 = sparse.rand(15000, 15000, density=0.0002, format='lil')
+sparse3 = sparse.rand(15000, 15000, density=0.0002, format='lil')
+addition = sparse.rand(15000, 15000, density=0.0002, format='lil')
+
+# n = [[int(OD_matrix[n][0]), OD_matrix[n][1], int(OD_matrix[n][2]), 'path', 'weight_array'] for n in range(0, len(OD_matrix))]
+n = [[0,1,2,'string','string',sparse1,sparse1] for n in range(0,10086)]
+
+# unique_links=[[400,600],[500,70],[90,4],[2345,9824],[10234,19],[9543,25],[0,0],[1,2],[12345,7644],[2345,9876]]
+# for unique_link in unique_links:
+#     sparse1[unique_link[0], unique_link[1]]=10
+#     sparse2[unique_link[0], unique_link[1]]=10
+#     sparse3[unique_link[0], unique_link[1]]=10
+#     addition[unique_link[0], unique_link[1]] = 10
+
+# addition=sparse.csr_array(addition)
+
+
+# time1 = time.time()
+# sparse1=sparse.csr_array(sparse1)
+# sparse2=sparse.csr_array(sparse2)
+# sparse3 = sparse.csr_array(sparse3)
+# weight_array_iter = sparse3.multiply(sparse1.multiply(sparse2.power(-1)).power(beta_array).multiply(alpha_array)+addition)
+# time2=time.time()
+
+# sparse1=sparse.lil_array(sparse1)
+# sparse2=sparse.lil_array(sparse2)
+# sparse3 = sparse.lil_array(sparse3)
+
+# time3=time.time()
+# weight_array_iter = sparse.lil_array(sparse3)
+# for unique_link in unique_links:
+#     weight_array_iter[unique_link[0], unique_link[1]] = sparse3[
+#         unique_link[0], unique_link[1]] * (1+alpha_array*(sparse1[unique_link[0], unique_link[1]]/sparse2[unique_link[0], unique_link[1]])**beta_array)
+# time4=time.time()
+
+# print(time2-time1)
+# print(time4-time3)
 
 
 
+# # parallelization attempt
+# # Create two sparse matrices
+# sparse1 = sparse.rand(15000, 15000, density=0.0002, format='csr')
+# sparse2 = sparse.rand(15000, 15000, density=0.0002, format='csr')
+# sparse3 = sparse.rand(15000, 15000, density=0.0002, format='csr')
+# addition = sparse.rand(15000, 15000, density=0.0002, format='csr')
+
+# # sparse1 = sparse.lil_array(data1,dtype='float32')
+# # sparse2 = sparse.lil_array(data2,dtype='float32')
+# # sparse3 = sparse.lil_array(data3, dtype='float32')
+# # addition = addition.tolil()
+
+
+# # Define the number of processes to use
+# num_processes = 2
+
+# def multiply_matrices(args):
+#     sparse1a, sparse2a, sparse3a, additiona = args
+#     c = sparse1a.multiply(sparse2a.power(-1)).power(beta_array).multiply(alpha_array)
+#     d=c+additiona
+#     weight_array_iter = sparse3a.multiply(d)
+#     return weight_array_iter
+
+# time1=time.time()
+# # Create a pool of processes
+# pool = Pool(processes=num_processes)
+# # Split the matrices into chunks based on the number of processes
+# chunks = [(sparse1[i::num_processes], 
+#            sparse2[i::num_processes],
+#            sparse3[i::num_processes],
+#            addition[i::num_processes]) for i in range(num_processes)]
+# time1a=time.time()
+# # Use the pool to parallelize the matrix multiplication
+# results = pool.map(multiply_matrices, chunks)
+# time1b=time.time()
+# # Combine the results into a single sparse matrix
+# result = sparse.vstack(results)
+# # Close the pool of processes
+# pool.close()
+# pool.join()
+
+# time2=time.time()
+
+# c = sparse1.multiply(sparse2.power(-1)).power(beta_array).multiply(alpha_array)
+# d = c+addition
+# weight_array_iter = sparse3.multiply(d)
+# time3=time.time()
+
+
+# print(time1a-time1)
+# print(time1b-time1a)
+# print(time2-time1b)
+# print(time2-time1)
+# print(time3-time2)
 
 
 
--- testing.py
+++ testing.py
@@ -14,6 +14,9 @@ import random
 import pandas as pd
 import rasterio
 import time
+import cProfile
+import pstats
+from pstats import SortKey
 pd.set_option('display.max_rows', None)
 
 # CAPUTRE THE SHAPELY DEPRECIAITION WARNINGS
@@ -30,7 +33,8 @@ ox.config(use_cache=True)
 f_path = '/home/mdp0023/Desktop/external/Data/Network_Data/AOI_Testing'
 f_boundaries = f'{f_path}/AOI_Boundary'
 f_food_shp = f'{f_path}/AOI_Food_Mart_Shapefiles'
-f_other_shp = f'{f_path}/AOI_Other_Resource_Shapefiles'
+f_other_shp2 = f'{f_path}/AOI_Other_Resource_Shapefiles'
+f_other_shp = f'{f_path}/AOI_Other_Resource2_Shapefiles'
 f_graphs = f'{f_path}/AOI_Graphs'
 f_parcel_access_shp = f'{f_path}/AOI_Parcel_Access_Shapefiles'
 f_res_shp = f'{f_path}/AOI_Residental_Parcel_Shapefiles'
@@ -43,11 +47,13 @@ aoi_buffer = f'{f_boundaries}/Neighborhood_Network_AOI_Buf_1km.shp'
 # Centroids of res parcels, food marts, and 2nd resource
 res_points_loc = f'{f_res_shp}/Residential_Parcels_Points_Network_AOI.shp'
 food_points_loc = f'{f_food_shp}/Food_Marts_Points_Network_AOI.shp'
-other_points_loc = f'{f_other_shp}/Other_Resource_Points_Network_AOI.shp'
+other_points_loc = f'{f_other_shp}/Other_Resource2_Points_Network_AOI.shp'
+other_points_loc2 = f'{f_other_shp2}/Other_Resource_Points_Network_AOI.shp'
 # Shapefiles of res parcels, food marts, and 2nd resource
 res_parcels = f'{f_res_shp}/Residential_Parcels_Network_AOI.shp'
 food_parcels = f'{f_food_shp}/Food_Marts_Network_AOI.shp'
-other_points_loc = f'{f_other_shp}/Other_Resource_Network_AOI.shp'
+other_parcels = f'{f_other_shp}/Other_Resource2_Network_AOI.shp'
+other_parcels2 = f'{f_other_shp2}/Other_Resource_Network_AOI.shp'
 # Inundation raster
 inundation_raster = f'{f_path}/AOI_Inundation.tif'
 # raster = rio.open(inundation_raster)
@@ -61,7 +67,6 @@ mynet.crs_check(source=aoi_area, shapefiles=[
 
 
 # read in files 
-# LOAD OTHER DATA ############################################################
 # shapefile centroids of residental plots
 res_points = gpd.read_file(res_points_loc)
 # shapefile of res parcels
@@ -70,83 +75,185 @@ res_locs = gpd.read_file(res_parcels)
 food_points = gpd.read_file(food_points_loc)
 # shapefile of food mart parcels
 food_locs = gpd.read_file(food_parcels)
+# shapefile centroids of other resource
+other_points = gpd.read_file(other_points_loc)
+# shapefile of other resource parcels
+other_locs = gpd.read_file(other_parcels)
 # shapefile of area of interest
 aoi_area = gpd.read_file(aoi_area)
 
+# shapefile centroids of other resource
+other_points2 = gpd.read_file(other_points_loc2)
+# shapefile of other resource parcels
+other_locs2 = gpd.read_file(other_parcels2)
 
 # FUNCTION TESTING
 
 # shape_2_graph
-network = mynet.shape_2_graph(aoi_buffer)
+# network = mynet.shape_2_graph(aoi_buffer)
 
 # save_2_disk
-mynet.save_2_disk(G=network, path=f_graphs, name='AOI_Graph')
+# mynet.save_2_disk(G=network, path=f_graphs, name='AOI_Graph')
 
 # read_graph_from_disk
 network = mynet.read_graph_from_disk(path=f_graphs, name='AOI_Graph')
-
+# ox.io.save_graph_geopackage(network, filepath='/home/mdp0023/Desktop/external/Data/Network_Data/AOI_Testing/test')
 # print list of all the edge attributes available to us
 print(list(list(network.edges(data=True))[0][-1].keys()))
 
-# parallel_edges
-parallel_edges, self_loop_edges = mynet.parallel_edges(network)
+# # parallel_edges
+# parallel_edges, self_loop_edges = mynet.parallel_edges(network)
 
-# nearest_nodes
-output1 = mynet.nearest_nodes(G=network, res_points=res_points, dest_points=food_points)
+# # nearest_nodes, single destination
+# output1a = mynet.nearest_nodes(G=network, res_points=res_points, dest_points=food_points)
+# # nearest_nodes, multiple destinations
+# output1b = mynet.nearest_nodes(G=network, res_points=res_points, dest_points=[food_points,other_points])
 
-# nearest_nodes_vertices
-output2 = mynet.nearest_nodes_vertices(network, res_points, food_locs)
+# print(output1a[1])
+# print(output1b[1])
+# print(output1a[2])
+# print(output1b[2])
+# print(output1a[3])
+# print(output1b[3])
+# print(output1a[4])
+# print(output1b[4])
 
-# random_shortest_path
-paths = mynet.random_shortest_path(network, res_points, food_points, plot=False)
+# nearest_nodes_vertices, single destination
+# output2a = mynet.nearest_nodes_vertices(network, res_points, food_locs, food_points)
+# # nearest_nodes_vertices, multiple destinations
+# output2b = mynet.nearest_nodes_vertices(network, res_points, [food_locs, other_locs], [food_points, other_points])
 
-# min_cost_flow_parcels
-output3 = mynet.min_cost_flow_parcels(network, res_points, food_points, food_locs, dest_method='multiple')
 
-# max_flow_parcels
-output4 = mynet.max_flow_parcels(network, res_points, food_points, food_locs, dest_method='multiple')
+# # # # random_shortest_path
+# # paths = mynet.random_shortest_path(network, res_points, food_points, plot=False)
+
+# MIN_COST_FLOW_PARCELS
+# single destination file, dest_method='single'
+# output3a = mynet.min_cost_flow_parcels(network, res_points, food_points, food_locs, dest_method='single')
+# # multiple destination files, dest_method='single'
+output3b = mynet.min_cost_flow_parcels(network, res_points, [food_points, other_points], [food_locs, other_locs], dest_method='single')
+# # single destination file, dest_method='multiple'
+output3c = mynet.min_cost_flow_parcels(network, res_points, food_points, food_locs, dest_method='multiple')
+# # # multiple destination files, dest_method='multiple'
+output3d = mynet.min_cost_flow_parcels(network, res_points, [food_points, other_points], [food_locs, other_locs], dest_method='multiple')
+# print(output3b[1])
+# print(output3c[1])
+# print(output3d[1])
+# # # # relate output3a/b/c/d to network to plot
+# for i in output3a[0]:
+#     for j in output3a[0][i]:
+#         nx.set_edge_attributes(network, {(i,j,0): {'test_flow1':output3a[0][i][j]}})
+# # # for i in output3b[0]:
+# # #     for j in output3b[0][i]:
+# # #         nx.set_edge_attributes(network, {(i,j,0): {'test_flow2':output3b[0][i][j]}})
+# # # for i in output3c[0]:
+# # #     for j in output3c[0][i]:
+# # #         nx.set_edge_attributes(network, {(i,j,0): {'test_flow3':output3c[0][i][j]}})
+# # # for i in output3d[0]:
+# # #     for j in output3d[0][i]:
+# # #         nx.set_edge_attributes(network, {(i,j,0): {'test_flow4':output3d[0][i][j]}})      
+
+#MAX_FLOW_PARCELS
+# single destination file, dest_method='single'
+# output4a = mynet.max_flow_parcels(network, res_points, food_points, food_locs, dest_method='single')
+# # multiple destination files, dest_method='single'
+# output4b = mynet.max_flow_parcels(network, res_points, [food_points, other_points], [food_locs, other_locs], dest_method='single')
+# # single destination file, dest_method='mulitple'
+# output4c = mynet.max_flow_parcels(network, res_points, food_points, food_locs, dest_method='multiple')
+# # multiple destination files, dest_method='multiple'
+# output4d = mynet.max_flow_parcels(network, res_points, [food_points, other_points], [food_locs, other_locs], dest_method='multiple')
+
 
 # plot_aoi -> lots can be added to this function
-# mynet.plot_aoi(network, res_locs, food_locs)
+# mynet.plot_aoi(network, res_locs, food_locs, edge_width='test_flow1')
+# # mynet.plot_aoi(network, res_locs, [food_locs, other_locs], edge_width='test_flow2')
+# # mynet.plot_aoi(network, res_locs, food_locs, edge_width='test_flow3')
+# # mynet.plot_aoi(network, res_locs, [food_locs, other_locs], edge_width='test_flow4')
+# plt.show()
 
 # summary_function -> lots can be added to this function in the future
 # output5 = mynet.summary_function(network)
 
-# inundate_network
+# # inundate_network
 # inundated_net = mynet.inundate_network(network, f_graphs, inundation_raster)
 # print(list(list(inundated_net.edges(data=True))[0][-1].keys()))
 
-# flow_decomposition
-# output6 = mynet.flow_decomposition(network, res_points, food_points, res_locs, food_locs, dest_method='multiple')
+# FLOW_DECOMPOSITION
+# single destination file, dest_method='single'
+# output6a = mynet.flow_decomposition(network, 
+#                                     res_points, 
+#                                     food_points, 
+#                                     res_locs, 
+#                                     food_locs, 
+#                                     dest_method='single')
+# multiple destination files, dest_method='single
+# output6b = mynet.flow_decomposition(network, 
+#                                     res_points, 
+#                                     [food_points, other_points], 
+#                                     res_locs, 
+#                                     [food_locs, other_locs], 
+#                                     dest_method='single')
+# # # single destination file, dest_method='multiple'
+# output6c = mynet.flow_decomposition(network,res_points_c
+#                                     res_points,
+#                                     food_points,
+#                                     res_locs,
+#                                     food_locs,
+#                                     dest_method='multiple')
+# # # multiple destination files, dest_method='multiple
+# # output6d = mynet.flow_decomposition(network,
+# #                                     res_points,
+# #                                     [food_points, other_points],
+# #                                     res_locs,
+# #                                     [food_locs, other_locs],
+# #                                     dest_method='multiple')
 
-# traffic_assignment
-# start=time.time()
 
-output7 = mynet.traffic_assignment(network, 
+# # # traffic_assignment
+# # # start=time.time()
+
+output7a = mynet.traffic_assignment(network, 
                                    res_points, 
                                    food_points, 
                                    food_locs, 
                                    dest_method='multiple',
-                                   termination_criteria=['iter',1], 
+                                   termination_criteria=['iter',3], 
                                    algorithm='path_based',
                                    method='CFW',
                                    sparse_array=True)
+
+output7b = mynet.traffic_assignment(network,
+                                    res_points,
+                                    [food_points, other_points, other_points2],
+                                    [food_locs, other_locs, other_locs2],
+                                    dest_method='multiple',
+                                    termination_criteria=['iter', 3],
+                                    algorithm='path_based',
+                                    method='CFW',
+                                    sparse_array=True)
+
 # end=time.time()
 # print(f"Time to compute: {end-start}")
 
 
-tstt1 = [round(num, 6) for num in output7[2]]
-sptt1 = [round(num, 6) for num in output7[3]]
-rg1 = [round(num, 6) for num in output7[4]]
+tstta = [round(num, 6) for num in output7a[2]]
+sptta = [round(num, 6) for num in output7a[3]]
+rga = [round(num, 6) for num in output7a[4]]
+
+tsttb = [round(num, 6) for num in output7b[2]]
+spttb = [round(num, 6) for num in output7b[3]]
+rgb = [round(num, 6) for num in output7b[4]]
+
+print(tstta)
+print(sptta)
+print(rga)
+
+print(tsttb)
+print(spttb)
+print(rgb)
 
 
+# print(list(list(output7[0].edges(data=True))[0][-1].keys()))
 
-print(tstt1)
-print(sptt1)
-
-
-
-print(list(list(output7[0].edges(data=True))[0][-1].keys()))
-
-mynet.plot_aoi(output7[0], res_locs, food_locs, edge_width='TA_Flow')
+mynet.plot_aoi(output7b[0], res_locs, [food_locs, other_locs, other_locs2], edge_width='TA_Flow')
 plt.show()
